353e65fec31d143525e9d66b8539dcacbc6f902f
gyungmean <yogurt2819@gmail.com>
2021-09-11 04:32:46 +0900

    week02_1

---
+++ week02/Q2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+w = int(input('enter the width: '))
+h = int(input('enter the height: '))
+print(w * h)
---
+++ week02/Q3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+season = {'spring', 'summer', 'fall', 'winter'}
+for w in season:
+	print(w)
