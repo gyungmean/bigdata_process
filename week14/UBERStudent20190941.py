82309ace5806623e3d1844e2ba138635c7ab9e4c
gyungmean <yogurt2819@gmail.com>
2021-12-09 00:07:14 +0900

    uber change

--- week14/UBERStudent20190941.py
+++ week14/UBERStudent20190941.py
@@ -25,16 +25,28 @@ while True:
 
 	data[info[0]] = {info[1] : [info[2], info[3]]}
 
-#print(data)
 
+#print(data)
 f1.close()
 
+result = {}
 f2 = open(output_file, 'w')
 for base_num, value in data.items():
+	if result.get(base_num) == None:
+		result[base_num] = {}
+
 	for date, value2 in value.items():
 		date2 = date.split('/')
 		day = get_day(int(date2[2]), int(date2[0]), int(date2[1]))
-		f2.write(base_num + "," + day + " " + value2[0] + "," + value2[1] + "\n")
+		if result[base_num].get(day) != None:
+			result[base_num][day][0] = int(result[base_num][day][0]) + int(value2[0]) 
+			result[base_num][day][1] = int(result[base_num][day][1]) + int(value2[1])
+		else:
+			result[base_num][day] = [value2[0], value2[1]]
+		
 
+for base_num, value in result.items():
+	for day, value2 in value.items():
+		f2.write(base_num + "," + day + " " + str(value2[0]) + "," + str(value2[1]) + "\n")
 	
 f2.close()
